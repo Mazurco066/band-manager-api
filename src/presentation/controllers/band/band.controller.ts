2ada9e93049f4c363497ebef695e69590482cc5d
Gabriel Mazurco <mazurco066@gmail.com>
2022-08-11 13:02:26 -0300

    refactor(controller): added band and invites controller[

--- src/presentation/controllers/band/band.controller.ts
+++ src/presentation/controllers/band/band.controller.ts
@@ -1,15 +1,114 @@
 // Dependencies
-import { Injectable, Controller} from '@nestjs/common'
-import { CommandBus, QueryBus } from '@nestjs/cqrs'
+import { Injectable, Controller, Param, Body, Query, Get, Post, Put, Delete } from '@nestjs/common'
+import { BandService } from '../../services/band'
+import { IBaseResponse } from '@/domain/shared'
+
+// Authorization
+import { Roles, Role, JwtUserDecorator } from '@/main/decorators'
+import { TokenPayload } from '@/data/protocols'
+
+// Inputs
+import {
+  AddBandInput,
+  AddMemberInput,
+  PromoteMemberInput,
+  RemoveMemberInput,
+  DemoteMemberInput,
+  UpdateBandInput,
+  ListBandsInput
+} from '@/domain/protocols'
 
 @Controller('api/v1/bands')
 @Injectable()
 export class BandController {
   // Dependencies Injection
   constructor(
-    private readonly commandBus: CommandBus,
-    private readonly queryBus: QueryBus
+    private readonly bandService: BandService
   ) {}
 
- 
+  @Get('/:id')
+  @Roles(Role.player, Role.master)
+  async loadBandById(
+    @Param('id') id: string,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.loadBandById(id, payload)
+  }
+
+  @Get()
+  @Roles(Role.player, Role.master)
+  async listBands(
+    @Query() params: ListBandsInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.listBands(params, payload)
+  }
+
+  @Post()
+  @Roles(Role.player, Role.master)
+  async addBand(
+    @Body() params: AddBandInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.addBand(params, payload)
+  }
+
+  @Put('/:id')
+  @Roles(Role.player, Role.master)
+  async updateBand(
+    @Param('id') id: string,
+    @Body() params: UpdateBandInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.updateBand(id, params, payload)
+  }
+
+  @Delete('/:id')
+  @Roles(Role.player, Role.master)
+  async removeBand(
+    @Param('id') id: string,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.removeBand(id, payload)
+  }
+
+  @Post('/add_member/:id')
+  @Roles(Role.player, Role.master)
+  async addBandMember(
+    @Param('id') id: string,
+    @Body() params: AddMemberInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.addBandMember(id, params, payload)
+  }
+
+  @Post('/promote_member/:id')
+  @Roles(Role.player, Role.master)
+  async promoteBandMember(
+    @Param('id') id: string,
+    @Body() params: PromoteMemberInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.promoteBandMember(id, params, payload)
+  }
+
+  @Post('/remove_member/:id')
+  @Roles(Role.player, Role.master)
+  async removeBandMember(
+    @Param('id') id: string,
+    @Body() params: RemoveMemberInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.removeBandMember(id, params, payload)
+  }
+
+  @Post('/demote_member/:id')
+  @Roles(Role.player, Role.master)
+  async demoteBandMember(
+    @Param('id') id: string,
+    @Body() params: DemoteMemberInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.bandService.demoteBandMember(id, params, payload)
+  }
 }
--- src/presentation/controllers/band/invite.controller.ts
+++ src/presentation/controllers/band/invite.controller.ts
@@ -1,15 +1,39 @@
 // Dependencies
-import { Injectable, Controller} from '@nestjs/common'
-import { CommandBus, QueryBus } from '@nestjs/cqrs'
+import { Injectable, Controller, Body, Post, Get } from '@nestjs/common'
+import { InviteService } from '../../services/band'
+import { IBaseResponse } from '@/domain/shared'
+
+// Authorization
+import { Roles, Role, JwtUserDecorator } from '@/main/decorators'
+import { TokenPayload } from '@/data/protocols'
+
+// Inputs
+import {
+  RespondInviteInput
+} from '@/domain/protocols'
 
 @Controller('api/v1/invitations')
 @Injectable()
 export class InviteController {
   // Dependencies Injection
   constructor(
-    private readonly commandBus: CommandBus,
-    private readonly queryBus: QueryBus
+    private readonly inviteService: InviteService
   ) {}
 
- 
+  @Post('/respond')
+  @Roles(Role.player, Role.master)
+  async respondInvite(
+    @Body() params: RespondInviteInput,
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.inviteService.respondInvite(params, payload)
+  }
+
+  @Get('/pending_invites')
+  @Roles(Role.player, Role.master)
+  async pendingInvites(
+    @JwtUserDecorator() payload: TokenPayload
+  ): Promise<IBaseResponse> {
+    return this.inviteService.pendingInvites(payload)
+  }
 }
