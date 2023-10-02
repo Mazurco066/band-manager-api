9a2c2bcaf4196254bd53da43b6acc8dc473d5c6e
Gabriel Mazurco <mazurco066@gmail.com>
2023-10-02 12:23:05 -0300

    fix(band): fixed band creation as well

--- src/domain/protocols/band/inputs/add-band.input.ts
+++ src/domain/protocols/band/inputs/add-band.input.ts
@@ -1,5 +1,5 @@
 // Dependencies
-import { IsString, IsNotEmpty, MinLength, MaxLength } from 'class-validator'
+import { IsString, IsNotEmpty, MinLength, MaxLength, IsOptional } from 'class-validator'
 
 // API Documentation
 import { ApiProperty } from '@nestjs/swagger'
@@ -18,4 +18,11 @@ export class AddBandInput {
   @MaxLength(256, { message: 'Campo "description" deve conter no máximo 256 caracteres' })
   @ApiProperty({ type: String, required: true, example: 'My band description' })
 	description!: string
+
+  @IsOptional()
+  @IsString({ message: 'Campo "logo" deve ser do tipo String' })
+  @MinLength(2, { message: 'Campo "logo" deve conter no mínimo 2 caracteres' })
+  @MaxLength(640, { message: 'Campo "logo" deve conter no máximo 256 caracteres' })
+  @ApiProperty({ type: String, required: false, example: 'http://cdn.com/logo.png' })
+	logo?: string
 }
--- src/domain/protocols/band/inputs/update-band.input.ts
+++ src/domain/protocols/band/inputs/update-band.input.ts
@@ -9,20 +9,20 @@ export class UpdateBandInput {
 	@IsString({ message: 'Campo "title" deve ser do tipo String' })
   @MinLength(2, { message: 'Campo "title" deve conter no mínimo 2 caracteres' })
   @MaxLength(256, { message: 'Campo "title" deve conter no máximo 256 caracteres' })
-  @ApiProperty({ type: String, required: true, example: 'My Awesome band' })
+  @ApiProperty({ type: String, required: false, example: 'My Awesome band' })
 	title?: string
 
   @IsOptional()
 	@IsString({ message: 'Campo "description" deve ser do tipo String' })
   @MinLength(2, { message: 'Campo "description" deve conter no mínimo 2 caracteres' })
   @MaxLength(256, { message: 'Campo "description" deve conter no máximo 256 caracteres' })
-  @ApiProperty({ type: String, required: true, example: 'My band description' })
+  @ApiProperty({ type: String, required: false, example: 'My band description' })
 	description?: string
 
   @IsOptional()
 	@IsString({ message: 'Campo "logo" deve ser do tipo String' })
   @MinLength(2, { message: 'Campo "logo" deve conter no mínimo 2 caracteres' })
   @MaxLength(640, { message: 'Campo "logo" deve conter no máximo 256 caracteres' })
-  @ApiProperty({ type: String, required: true, example: 'https://cdn.com.br/logo.png' })
+  @ApiProperty({ type: String, required: false, example: 'https://cdn.com.br/logo.png' })
 	logo?: string
 }
