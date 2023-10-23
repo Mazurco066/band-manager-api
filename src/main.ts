4bd66a3f48d15c9483e504bdcc09ad33e79b73c4
Gabriel Mazurco <mazurco066@gmail.com>
2023-10-23 10:50:49 -0300

    build(1.8.0): build for 1.8.0

--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,7 @@ const app = async () => {
     const swaggerConfig = new DocumentBuilder()
       .setTitle('Playliter API')
       .setDescription('Rest API for playliter app')
-      .setVersion('1.7.2')
+      .setVersion('1.8.0')
       .build()
     const document = SwaggerModule.createDocument(app, swaggerConfig)
     SwaggerModule.setup('api', app, document)
